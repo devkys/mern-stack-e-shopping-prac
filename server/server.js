d3670e12db5d0c22357c970452bee4c51a42c90c
kyeongseo <keongseo1234@gmail.com>
2022-11-22 22:14:18 +0900

    data import

--- server/server.js
+++ server/server.js
@@ -1,8 +1,9 @@
-require('dotenv').config();
-const express = require('express')
+require("dotenv").config();
+const express = require("express");
 const app = express();
+const connectDB = require("./config/db");
+
+connectDB();
 
 const PORT = process.env.PORT || 5000;
-app.listen(PORT, () => 
-    console.log(`Server is running on ${PORT}`)
-)
+app.listen(PORT, () => console.log(`Server is running on ${PORT}`));

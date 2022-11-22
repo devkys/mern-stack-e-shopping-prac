5a0c8ff7bd58d4d0bb2a36332f8dabf573f9d87b
kyeongseo <keongseo1234@gmail.com>
2022-11-22 23:32:52 +0900

    navbar

--- public/src/components/Navbar.js
+++ public/src/components/Navbar.js
@@ -1,26 +1,35 @@
-import React from 'react'
-import './Navbar.css';
-import { Link } from 'react-router-dom';
+import React from "react";
+import "./Navbar.css";
+import { Link } from "react-router-dom";
 function Navbar() {
   return (
-    <div className='navbar'>
-      { /* logo */}
-      <div className='navbar__logo'>
+    <div className="navbar">
+      {/* logo */}
+      <div className="navbar__logo">
         <h2>MERN Shopping Cart</h2>
       </div>
-      { /* links */}
-      <ul className='navbar__links'>
+      {/* links */}
+      <ul className="navbar__links">
         <li>
           <Link to="/cart">
-
-
+            <i className="fas fa-shopping-cart"></i>
+            Cart
+            <span className="cartlogo__badge">0</span>
           </Link>
         </li>
+        <li>
+          <Link to="/">Shop</Link>
+        </li>
       </ul>
-      { /* hamburger menu */}
-
+      {/* hamburger menu */}
+      <div className="hamburger__menu">
+        <div></div>
+        <div></div>
+        <div></div>
+        <div></div>
+      </div>
     </div>
-  )
+  );
 }
 
-export default Navbar
+export default Navbar;
